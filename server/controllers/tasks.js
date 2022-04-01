daf44e42606ec401a08f6bb4a5e01374efc80b20
N02959575 <menam3@hawkmail.newpaltz.edu>
2022-04-01 14:46:04 -0400

    added same features from users to tasks

--- server/controllers/tasks.js
+++ server/controllers/tasks.js
@@ -10,10 +10,11 @@ app
         res.send(taskModel.tasks);
     })
     .post('/', (req, res , next) => {
-        taskModel.create(req.body)
-        .then(task => {
-            res.status(CREATED_STATUS).send(task);
-        }).catch(next);
+        const task = taskModel.create(req.body)
+        res.status(CREATED_STATUS).send(task);
+        //.then(task => {
+          //  res.status(CREATED_STATUS).send(task);
+        //}).catch(next);
     })
     .delete('/:id', (req,res) => {
         const task = taskModel.remove(req.params.id);
--- server/models/task.js
+++ server/models/task.js
@@ -1,18 +1,45 @@
+let highestId = 5;
+
 const tasks = [
-    { task: "Make Bulma great again", dueDate:"2022-4-19", creator: "@johndoe", taskee: "@obodoe", checked: true },
-    { task: "Add some more features", dueDate:"2022-4-8", creator: "@deborahdoe", taskee: "@johndoe", checked: false },
-    { task: "Make a github account", dueDate:"2022-4-3", creator: "@obodoe", taskee: "@deborahdoe", checked: false },
-    { task: " Learn how to use github", dueDate:"2022-4-10", creator: "@deborahdoe", taskee: "@johndoe", checked: false },
-    { task: "add a .gitignore file", dueDate:"2022-4-1", creator: "@obodoe", taskee: "@deborahdoe", checked: false },
+    { task: "Make Bulma great again", dueDate:"2022-4-19", creator: "@johndoe", taskee: "@obodoe", checked: true, id: 1 },
+    { task: "Add some more features", dueDate:"2022-4-8", creator: "@deborahdoe", taskee: "@johndoe", checked: false, id: 2 },
+    { task: "Make a github account", dueDate:"2022-4-3", creator: "@obodoe", taskee: "@deborahdoe", checked: false, id: 3 },
+    { task: " Learn how to use github", dueDate:"2022-4-10", creator: "@deborahdoe", taskee: "@johndoe", checked: false, id: 4 },
+    { task: "add a .gitignore file", dueDate:"2022-4-1", creator: "@obodoe", taskee: "@deborahdoe", checked: false, id: 5 },
   ]
 
 function create(task){
-    tasks.checked = false;
+    task.id = ++highestId;
+    task.checked = false;
+
+    //throw {message: 'This is a test error'};
 
     tasks.push(task);
     return task;
 }
 
+function remove(id){
+    const index = tasks.findIndex(task => task.id === parseInt(id));
+    const task = tasks.splice(index, 1);
 
-module.exports.tasks = tasks;
-module.exports.create = create;
+    return task[0];
+}
+
+function update(id, newTask){
+    const index = tasks.findIndex(task => task.id === parseInt(id));
+    const oldTask = tasks[index];
+    newTask = tasks[index] = {...oldTask, ...newTask};
+    console.log(newTask)
+    return newTask;
+}
+
+module.exports = {
+    create,
+    remove,
+    update,
+    get tasks(){
+        return tasks.map(task => {
+            return task;
+        });
+    }
+}
