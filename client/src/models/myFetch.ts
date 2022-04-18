0d2ab31755d7330c73c5b5d01fcf8b8721393a87
N02959575 <menam3@hawkmail.newpaltz.edu>
2022-04-18 16:59:07 -0400

    updated myFetch.ts to handle posts

--- client/src/models/myFetch.ts
+++ client/src/models/myFetch.ts
@@ -1,6 +1,19 @@
 const API_URL = 'http://localhost:3001/api/';
 
-export function api(url: string) {
-  return fetch(
-      API_URL + url).then(res => res.json());
+export function api(url: string, body?: any, method?: string, headers?: HeadersInit) {/////////////
+
+  let options: RequestInit = {};
+
+  if (body) {
+    options = {
+      method: method ?? 'POST', // Method or default to POST
+      cache: 'no-cache', // By default, don't cache when using POST
+      headers: {// Default headers
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body) // Default body
+    };
+  }
+
+  return fetch(API_URL + url, options).then(res => res.json());
 }
