4a67a2b2d6efbe3ca14f68c1826795d5a063668b
N02959575 <menam3@hawkmail.newpaltz.edu>
2022-04-18 17:01:39 -0400

    changed sessions.ts to pinia

--- client/src/models/session.ts
+++ client/src/models/session.ts
@@ -1,31 +1,71 @@
-import {reactive} from "vue";
+import { defineStore } from 'pinia';
+import { useMessages } from './messages';
 import router from "../router";
-
 import * as users from "../models/user";
+import { api } from './myFetch';
 
-const session =reactive({
-
+export const useSession = defineStore( 'session', {
+    state: () => ({
         user: null as users.User | null,
+        destinationUrl: null as string | null,
+    }),
     
-}) 
+    actions: {
+        async Login(email: string, password: string) {
+    
+            const messages = useMessages();
+        
+            try {
+                const user = await this.api("users/login", { email, password });
+        
+                if(user) {
+        
+                    messages.notifications.push({
+                        type: "success",
+                        message: `Welcome back ${user.firstName}`,
+                    });
+                
+                    this.user = user;
+                    router.push(this.destinationUrl ?? '/wall');
+                }
+                
+            } catch (error: any) {
+                
+                messages.notifications.push({
+                    type: "danger",
+                    message: error.message,
+                });
+            }
+            
+        },
 
-export async function Login(handle: string, password: string) {
-    const user = users.list.find(u => u.handle === handle);
-    if(!user){
-        throw {message: "User not found"}
-    }
-    if(user.password !== password){
-        throw {message: "Incorrect password"}
-    }
+        Logout(){
+            this.user = null;
+            router.push('/login')
+        },
 
-    session.user = user;
-    router.push('/tasktracker')
+        async api(url: string, data?: any, method?: 'GET' | 'POST' | 'PUT' | 'DELETE', headers?: any) {
+            const messages = useMessages();
+
+            try {
+                const response = await api(url, data, method, headers);
+                if(response.errors?.length) {
+                    throw {message: response.errors.join(', ')};
+                }
+                return await response.data;
+            } catch (error: any) {
+                messages.notifications.push({
+                    type: "danger",
+                    message: error.message,
+                });
+            }
+            
+        }
+    },
+})
+
+export interface ApiResult {
+    data: any;
+    errors?: string[];
+    success: boolean;
 }
-
-
-export function Logout(){
-    session.user = null;
-    router.push('/login')
-}
-
-export default session;
