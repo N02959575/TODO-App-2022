0ee8918fa776e90a72b9fde080958d9e6e50bad3
N02959575 <menam3@hawkmail.newpaltz.edu>
2022-05-01 09:37:16 -0400

    modified homepage

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -6,8 +6,68 @@
 
 <template>
     <div class="section">
-        <h1 class="title">Home Page!</h1>
+        <div class="container has-text-centered">
+            <p class="title">
+                TODOAPP
+            </p>
+            <p class="subtitle">
+                Your Task Managing System
+            </p>
+            <br>
+        </div>
+        <div class="tile is-ancestor">
+            <div class="tile is-parent">
+                <article class="tile is-child notification is-primary">
+                    <p class="title">For the Office!</p>
+                    <p class="subtitle">Keep your team organized.</p>
+                    <div class="card">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                            <img src="../assets/office 2.jpg" alt="Placeholder image">
+                            </figure>
+                        </div>
+                    </div>
+                </article>
+            </div>
+
+            <div class="tile is-parent">
+                <article class="tile is-child notification is-primary">
+                    <p class="title">For School!</p>
+                    <p class="subtitle">Stay on track to reach your goal.</p>
+                    <div class="card">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                            <img src="../assets/school.jpg" alt="Placeholder image">
+                            </figure>
+                        </div>
+                    </div>
+                </article>
+            </div>
+                
+            <div class="tile is-parent">
+                <article class="tile is-child notification is-primary">
+                    <p class="title">For the Home!</p>
+                    <p class="subtitle">Leave no chore forgotten.</p>
+                    <div class="card">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                            <img src="../assets/home.jpg" alt="Placeholder image">
+                            </figure>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </div>
+        <footer class="footer">
+            <div class="content has-text-centered">
+                <p>
+                    <strong>TODOAPP</strong> by <a href="https://github.com/N02959575/TODO-App-2022">Mario Mena</a>. A web project
+                    for Advance Web Programming.
+                </p>
+            </div>
+        </footer>
     </div>
+    
 </template>
 
 <style scoped>
